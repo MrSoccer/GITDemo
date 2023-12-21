bc4e8bd4b4c553c002b342df41a9ce7ec4253ab2
lordbagel <58605060+lordbagel42@users.noreply.github.com>
2023-12-20 20:58:46 -0700

    added prompt logic, sets username variable

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        String username = "";
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please enter your name");
+        String username = input.nextLine();
         System.out.println("Hello, Sawyer and Raygen!");
 
         System.out.println( "Welcome to class");
